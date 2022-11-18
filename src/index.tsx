53a66e75ab9f60c3f538fcfef4f061df98627c2b
Junting <342766475@qq.com>
2022-11-18 16:27:22 +0800

    feat: react-query-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import {
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import App from './app';
 
@@ -21,6 +22,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
